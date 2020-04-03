7f1c1be2f0505540d79bd068f1d562dc32921b2d
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-03 10:43:23 -0700

    Fix display to show messages. message 1 only working now

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,37 @@
-
-
 function dispNum(number){
   var numArray = [];
   for(i=0; i<=number; i++){
-   numArray.push(i);
-  };
-  numArray[1] = '"Beep!"'
-  numArray[2] = '"Boop!"'
-  numArray[3] = '"Won\'t you be my neighbor?"'
+    numArray.push(i);
+  }
   return numArray;
 };
+
+function beep(number){
+  var dispNumArray = dispNum(number);
+    if(dispNumArray.includes(1)){
+      dispNumArray[1] = '"Beep!"'
+    } else if (dispNumArray.includes(2)){
+      console.log('hi')
+      dispNumArray[1] = '"Beep!"'
+      dispNumArray[2] = '"Boop!"'
+    } else if (dispNumArray.includes(3)){
+      dispNumArray[1] = '"Beep!"'
+      dispNumArray[2] = '"Boop!"'
+      dispNumArray[3] = '"Won\'t you be my neighbor?"'
+    }
+  return dispNumArray;
+}
 
 $(document).ready(function(){
   $("#form-container").submit(function(event){
     event.preventDefault();
     var userInput = $("#user-input").val();
-    if(userInput == 1){
-      userInput = "Beep!"
-    } else if (userInput == 2){
-      userInput = "Boop1"
-    }
-
     var answerArray = dispNum(userInput);
    
-    $(".answer").text(answerArray);
-    console.log(answerArray)
+    var beepMessage = beep(userInput);
+
+    $(".answer").text(beepMessage);
+    console.log(answerArray);
+    console.log(beepMessage);
   });
 });
