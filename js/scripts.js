bf540c5be7ca70ae05c14eeb6ec68e01decae81a
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-03 11:10:36 -0700

    Fix and add functions to displays messages through 0 to 9

--- js/scripts.js
+++ js/scripts.js
@@ -8,26 +8,24 @@ function dispNum(number){
 
 function beep(number){
   var dispNumArray = dispNum(number);
-    if(dispNumArray.includes(1)){
+  if(dispNumArray.includes(1)){
       dispNumArray[1] = '"Beep!"'
-    } else if (dispNumArray.includes(2)){
-      console.log('hi')
-      dispNumArray[1] = '"Beep!"'
-      dispNumArray[2] = '"Boop!"'
-    } else if (dispNumArray.includes(3)){
-      dispNumArray[1] = '"Beep!"'
-      dispNumArray[2] = '"Boop!"'
-      dispNumArray[3] = '"Won\'t you be my neighbor?"'
-    }
+  }
+  if(dispNumArray.includes(2)){
+    dispNumArray[2] = '"Boop!"'
+  }
+  if(dispNumArray.includes(3)){
+    dispNumArray[3] = '"Won\'t you be my neighbor?!"'
+  }
   return dispNumArray;
 }
+
 
 $(document).ready(function(){
   $("#form-container").submit(function(event){
     event.preventDefault();
     var userInput = $("#user-input").val();
     var answerArray = dispNum(userInput);
-   
     var beepMessage = beep(userInput);
 
     $(".answer").text(beepMessage);
