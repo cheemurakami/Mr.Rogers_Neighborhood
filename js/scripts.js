6d488e4b1cf3c822fa762cb3955c55861e95cb19
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-03 16:43:12 -0700

    Shows messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,46 @@
 function dispNum(number){
   var numArray = [];
   for(var i=0; i<=number; i++){
-    numArray.push(i);
+    if(i === 1) {
+       numArray.push("Beep!")
+    } else {
+      numArray.push(i);
+    }
   } 
   return numArray;
 };
 
+// function beep(number){
+//   var dispNumArray = [];
+//   for(var i = 0; i<number; i++){
+//     if (i === 1){
+//       var index = dispNumArray.indexOf(1)
+//       dispNumArray[index] = '"Beep!"'
+//     } else if (i === 2){
+//       var index = dispNumArray.indexOf(2)
+//       dispNumArray[index] = '"Boop!"'
+//     } else if (i === 3){
+//       var index = dispNumArray.indexOf(3)
+//       dispNumArray[index] = '"Won\'t you be my neighbor?"'
+//     }
+//   }
+//   return dispNumArray;
+// }
+
 function beep(number){
-  var dispNumArray = dispNum(number);
-  for(var i = 0; i<dispNumArray.length; i++){
-    if (dispNumArray.includes(1)){
-      var index = dispNumArray.indexOf(1)
-      dispNumArray[index] = '"Beep!"'
-    } else if (dispNumArray.includes(2)){
-      var index = dispNumArray.indexOf(2)
-      dispNumArray[index] = '"Boop!"'
-    } else if (dispNumArray.includes(3)){
-      var index = dispNumArray.indexOf(3)
-      dispNumArray[index] = '"Won\'t you be my neighbor?"'
+  var dispNumArray = [];
+  for(var i = 0; i<=number; i++){
+    console.log(i)
+    if (i.toString().indexOf(3) !== -1){
+      dispNumArray.push('"Won\'t you be my neighbor?"')
+    } else if (i.toString().indexOf(2) !== -1){
+      dispNumArray.push('"Boop!"')
+    } else if (i.toString().indexOf(1) !== -1){
+      dispNumArray.push('"Beep!"')
+    } else {
+      dispNumArray.push(i)
     }
   }
-  // if(dispNumArray.includes(1)){
-  //     dispNumArray[1] = '"Beep!"'
-  // }
-  // if(dispNumArray.includes(2)){
-  //   dispNumArray[2] = '"Boop!"'
-  // }
-  // if(dispNumArray.includes(3)){
-  //   dispNumArray[3] = '"Won\'t you be my neighbor?"'
-  // }
   return dispNumArray;
 }
 
@@ -41,27 +53,17 @@ function overTen(number){
   }
   var numStArray = numOverTenArray; 
     for(var i = 0; i<numStArray.length; i++){
-      // var placeArray = numStArray[i].toString().split('')
-      if(numStArray.includes(1)){
-        numStArray[i] = '"Beep!"'
+      var placeArray = numStArray[i].toString().split('')
+      if(placeArray.includes(1)){
+        placeArray[i] = '"Beep!"'
       } else if (numStArray.includes(2)){
         numStArray[i] = '"Boop!"'
       } else if (numStArray.includes(3)){
         numStArray[i] = '"waaa!"'
       }
-
-      // if((placeArray[0] == 1) && (placeArray[1] < 1)){
-      //   numStArray[i] = '"Beep!"'
-      // }
-      // if ((placeArray[0] == 1) && (placeArray[1] < 2)){
-      //   numStArray[i] = '"Beep!"'
-      // }
-      // if ((placeArray[0] == 1) && (placeArray[1] < 3)){
-      //   numStArray[i] = '"Boop!"'
-      // } 
     }
       
-  return numStArray;
+  return placeArray;
   }
 
 
@@ -70,7 +72,7 @@ function overTen(number){
 $(document).ready(function(){
   $("#form-container").submit(function(event){
     event.preventDefault();
-    var userInput = ($("#user-input").val());
+    var userInput = parseInt(($("#user-input").val()));
     var answerArray = dispNum(userInput);
     var beepMessage = beep(userInput);
     var overTenMessage = overTen(userInput);
