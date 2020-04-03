41ea5987f52221628bdd29bd540be342bb48455e
Chisato Murakami <kiwipom1031@gmail.com>
2020-04-03 09:52:38 -0700

    Add messages correspond to the number 1,2, and 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
+
+
 function dispNum(number){
   var numArray = [];
   for(i=0; i<=number; i++){
    numArray.push(i);
-  }
+  };
+  numArray[1] = '"Beep!"'
+  numArray[2] = '"Boop!"'
+  numArray[3] = '"Won\'t you be my neighbor?"'
   return numArray;
-}
-
-
+};
 
 $(document).ready(function(){
   $("#form-container").submit(function(event){
     event.preventDefault();
     var userInput = $("#user-input").val();
-    var answer = dispNum(userInput);
-    $(".answer").text(answer);
+    if(userInput == 1){
+      userInput = "Beep!"
+    } else if (userInput == 2){
+      userInput = "Boop1"
+    }
+
+    var answerArray = dispNum(userInput);
+   
+    $(".answer").text(answerArray);
+    console.log(answerArray)
   });
 });
